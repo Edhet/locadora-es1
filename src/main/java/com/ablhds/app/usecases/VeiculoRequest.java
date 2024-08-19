f390e7d0f9926320f4b8e22b1a917b1ad3667174
RonAlffrson <renan.c.alves@edu.unirio.br>
2024-08-18 22:25:21 -0300

    DTO Veiculo implementado

--- src/main/java/com/ablhds/app/usecases/VeiculoRequest.java
+++ src/main/java/com/ablhds/app/usecases/VeiculoRequest.java
@@ -1,4 +1,9 @@
 package com.ablhds.app.usecases;
 
-public record VeiculoRequest() {
+public record VeiculoRequest(String id,
+                             String placa,
+                             String modelo,
+                             Integer ano,
+                             Double diaria,
+                             Integer quilometragem) {
 }
