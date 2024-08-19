b70b421806dfb2df867d2d68992d79c9f83afa48
RonAlffrson <renan.c.alves@edu.unirio.br>
2024-08-18 22:23:35 -0300

    Mapper Veiculo implementado

--- src/main/java/com/ablhds/app/persistence/VeiculoMapper.java
+++ src/main/java/com/ablhds/app/persistence/VeiculoMapper.java
@@ -1,4 +1,17 @@
 package com.ablhds.app.persistence;
 
+import com.ablhds.app.domain.dao.VeiculoDTO;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class VeiculoMapper {
+    public VeiculoDTO map(ResultSet rs) throws SQLException {
+        return new VeiculoDTO(rs.getString("id"),
+                rs.getString("placa"),
+                rs.getString("modelo"),
+                rs.getInt("ano"),
+                rs.getDouble("diaria"),
+                rs.getInt("quilometragem"));
+    }
 }
