707a461aa6bfc7bcd4325d458b18f2845733908d
RonAlffrson <renan.c.alves@edu.unirio.br>
2024-08-18 22:26:29 -0300

    Controllers Veiculo implementados

--- src/main/java/com/ablhds/app/usecases/IncluirVeiculoCtrl.java
+++ src/main/java/com/ablhds/app/usecases/IncluirVeiculoCtrl.java
@@ -1,4 +1,45 @@
 package com.ablhds.app.usecases;
 
+import com.ablhds.app.domain.*;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public class IncluirVeiculoCtrl {
+    private final VeiculoRepository repo;
+
+    public IncluirVeiculoCtrl(VeiculoRepository repo) {
+        super();
+        this.repo = repo;
+    }
+
+    public List<Erro> incluirVeiculo(VeiculoRequest request) {
+        try {
+            var resultado = new VeiculoBuilder()
+                    .withPlaca(request.placa())
+                    .withModelo(request.modelo())
+                    .withAno(request.ano())
+                    .withDiaria(request.diaria())
+                    .withQuilometragem(request.quilometragem())
+                    .build();
+
+            if (resultado.sucesso()) {
+                var veiculo = resultado.valor;
+
+                var outroVeiculo = repo.findByPlaca(veiculo.getPlaca().valor);
+
+                if (outroVeiculo != null) {
+                    return List.of(Erro.VEICULO_PLACA_DUPLICADA);
+                }
+
+                repo.add(veiculo);
+
+                return null;
+            } else {
+                return resultado.erros;
+            }
+        } catch (SQLException e) {
+            return List.of(Erro.ERRO_BD);
+        }
+    }
 }
--- src/main/java/com/ablhds/app/usecases/ListarVeiculosCtrl.java
+++ src/main/java/com/ablhds/app/usecases/ListarVeiculosCtrl.java
@@ -1,4 +1,26 @@
 package com.ablhds.app.usecases;
 
+import com.ablhds.app.domain.*;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public class ListarVeiculosCtrl {
+    private final VeiculoRepository repo;
+
+    public ListarVeiculosCtrl(VeiculoRepository repo) {
+        super();
+        this.repo = repo;
+    }
+
+    public Resultado<List<Veiculo>> recuperarTodosVeiculos() {
+        try {
+            var veiculos = repo.findAll();
+
+            return Resultado.ok(veiculos);
+        } catch (SQLException e) {
+            // Se ocorrer alguma exceção no BD, avisa
+            return Resultado.erro(List.of(Erro.ERRO_BD));
+        }
+    }
 }
