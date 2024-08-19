2b4ca510cf5b3f7ef590203a77455945e9e75d0e
RonAlffrson <renan.c.alves@edu.unirio.br>
2024-08-18 22:24:45 -0300

    DTO Veiculo implementado

--- src/main/java/com/ablhds/app/ui/VeiculoData.java
+++ src/main/java/com/ablhds/app/ui/VeiculoData.java
@@ -1,4 +1,9 @@
 package com.ablhds.app.ui;
 
-public record VeiculoData() {
+public record VeiculoData(String id,
+                          String placa,
+                          String modelo,
+                          Integer ano,
+                          Double diaria,
+                          Integer quilometragem) {
 }
